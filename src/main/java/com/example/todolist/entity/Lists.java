88860ce6dbfc3bc04a786bf1ee7dcafd7b28cfbf
Nataliya Gordiets <Natgord@rambler.ru>
2020-10-19 23:35:59 +0700

    getters, setters and constructors created

--- src/main/java/com/example/todolist/entity/Lists.java
+++ src/main/java/com/example/todolist/entity/Lists.java
@@ -1,10 +1,51 @@
 package com.example.todolist.entity;
 
+
 import java.time.LocalDate;
 
+/**
+ * Класс Списки дел
+ */
 public class Lists {
     private int listId;
     private String name;
     private LocalDate listCreated;
     private LocalDate listChange;
+
+    public Lists(String name) {
+        this.name = name;
+        this.listCreated = LocalDate.now();
+    }
+
+    public int getListId() {
+        return this.listId;
+    }
+
+//    public void setListId(int listId) {
+//        this.listId = listId;
+//    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getListCreated() {
+        return this.listCreated;
+    }
+
+//    public void setListCreated(LocalDate listCreated) {
+//        this.listCreated = listCreated;
+//    }
+
+    public LocalDate getListChange() {
+        return this.listChange;
+    }
+
+//    public void setListChange(LocalDate listChange) {
+//        this.listChange = listChange;
+//    }
 }
--- src/main/java/com/example/todolist/entity/Tasks.java
+++ src/main/java/com/example/todolist/entity/Tasks.java
@@ -2,13 +2,89 @@ package com.example.todolist.entity;
 
 import java.time.LocalDate;
 
+/**
+ * Класс дела
+ */
 public class Tasks {
     private int taskId;
     private String name;
     private String description;
-    private int priority;
+    private int priority;    // от 1 до 5
     private int markDone;    //1- сделано, 0 - нет
     private LocalDate taskCreated;
     private LocalDate taskChange;
 
+    public Tasks(String name) {
+        this.name = name;
+        this.taskCreated = LocalDate.now();
+        this.markDone = 0;
+    }
+    public Tasks(String name, String description){
+        this.name = name;
+        this.description = description;
+        this.taskCreated = LocalDate.now();
+        this.markDone = 0;
+    }
+    public Tasks(String name, String description, int priority){
+        this.name = name;
+        this.description = description;
+        this.priority = priority;
+        this.taskCreated = LocalDate.now();
+        this.markDone = 0;
+    }
+    public int getTaskId() {
+        return this.taskId;
+    }
+
+//    public void setTaskId(int taskId) {
+//        this.taskId = taskId;
+//    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPriority() {
+        return this.priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public int getMarkDone() {
+        return this.markDone;
+    }
+
+    public void setMarkDone(int markDone) {
+        this.markDone = markDone;
+    }
+
+    public LocalDate getTaskCreated() {
+        return this.taskCreated;
+    }
+//
+//    public void setTaskCreated(LocalDate taskCreated) {
+//        this.taskCreated = taskCreated;
+//    }
+
+    public LocalDate getTaskChange() {
+        return this.taskChange;
+    }
+//
+//    public void setTaskChange(LocalDate taskChange) {
+//        this.taskChange = taskChange;
+//    }
 }
